f780ea2a5de7387a29fba2efd953e365aa7cd090
Ruslan <itachi_7@mail.ru>
2024-07-20 14:51:05 +0300

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,5 @@
 # 1st program
-print((9*5)**0,5)
+print((9**0.5)*5)
 
 # 2st program
 print(9.99 > 9.98 and 1000!=1000.1)
@@ -15,11 +15,3 @@ z = 13.42
 v = 42.13
 
 print(int(z//1) == round((v-int(v))*100) or round(z-int(z))*100 == int(v//1))
-
-z1 = int(z)
-v1 = int(v)
-
-z2 = int(z1-z)*100
-v2 = int(v1-v)*100
-
-print(z2==v2)
