40de4c901d4e651be88eef4de9347ac16cd6a47a
Ruslan <itachi_7@mail.ru>
2024-07-21 17:52:31 +0300

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -7,17 +7,11 @@ print(9.99 > 9.98 and 1000!=1000.1)
 # 3rd program
 x = 1234
 y = 5678
-print(((x%1000)//10)+((y%1000)//10))
+print(((x % 1000)//10)+((y % 1000)//10))
 
 # 4th program
 
 z = 13.42
 v = 42.13
+
 print(int(z) == int((v-int(v))*100) and round(z%int(z)*100) == int(v))  # решение
-
-print(int(z//1) == round((v-int(v))*100) and round(z-int(z))*100 == int(v//1))    # пробовал другим способ решить
-
-print(int(z//1) == round((v-int(v))*100) and int(v) == int(v//1))   # вроде тоже верное решение
-
-
-
