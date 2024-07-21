860a2b72fe4297849ff02e32b440e2c2906ee25d
Ruslan <itachi_7@mail.ru>
2024-07-21 17:57:36 +0300

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -14,4 +14,5 @@ print(((x % 1000)//10)+((y % 1000)//10))
 z = 13.42
 v = 42.13
 
-print(int(z) == int((v-int(v))*100) and round(z%int(z)*100) == int(v))  # решение
+print(int(z) == int((v-int(v))*100) or round(z%int(z)*100) == int(v))  # решение
+
